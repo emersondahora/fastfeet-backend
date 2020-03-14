03692dbb9b79a3c0607a4e17da74f7c624958d9c
Emerson O da Hora <emersondahora@gmail.com>
2020-03-14 15:45:55 -0300

    Criaçao de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 import SessionControler from './app/controller/SessionController';
 import RecipientControler from './app/controller/RecipientController';
+import FileController from './app/controller/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 /** Public routes */
 routes.post('/session', SessionControler.store);
@@ -14,5 +18,7 @@ routes.use(authMiddleware);
 /** Private routes */
 routes.post('/recipients', RecipientControler.store);
 routes.put('/recipients/:id', RecipientControler.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
